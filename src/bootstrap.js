0a82b9dbf813633a61a957ca2d54dac327b76a38
Abdelrahmanee <123949503+Abdelrahmanee@users.noreply.github.com>
2024-09-14 21:33:12 +0300

    sjxbks

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,27 +4,64 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import v1Router from './routers/v1.routes.js'
-import { AppError } from './utilies/error.js'
+import { AppError, catchAsyncError } from './utilies/error.js'
 import { cron } from './utilies/cron.js'
 import { cloudinaryCofigration } from './utilies/cloudinary.js'
 import cookieParser from 'cookie-parser'
 import StripePaymentService from './modules/online-payment/services/online-payment.service.js'
 import { WebhookController } from './modules/online-payment/controllers/webhook.controller.js'
+import { makeOnlineOrder } from './modules/online-payment/controllers/online-payment.controller.js'
 
 
 export const bootstrap = (app) => {
 
-    const stripePaymentService = new StripePaymentService();
+    // const stripePaymentService = new StripePaymentService();
 
-    // Initialize the controller with dependency injection
-    const webhookController = new WebhookController(stripePaymentService);
+    // // Initialize the controller with dependency injection
+    // const webhookController = new WebhookController(stripePaymentService);
     
-    // Define the webhook route
-    app.post('/webhook', (req, res) => {
-        console.log("Webhook");
+    // // Define the webhook route
+    // app.post('/webhook', (req, res) => {
+    //     console.log("Webhook");
         
-      webhookController.handleWebhook(req, res);
-    });
+    //   webhookController.handleWebhook(req, res);
+    // });
+
+    app.post("/webhook",express.raw({ type: "application/json" }),
+        catchAsyncError(async (request, response) => {
+            const sig = request.headers["stripe-signature"];
+    
+            let event;
+    
+            try {
+                event = Stripe.webhooks.constructEvent(
+                    request.body,
+                    sig,
+                    process.env.WEB_HOOK_SECRET
+                );
+            } catch (err) {
+                response.status(400).send(`Webhook Error: ${err.message}`);
+                return;
+            }
+    
+            // Handle the event
+            switch (event.type) {
+                case "checkout.session.completed":
+                    const data = event.data.object;
+                    await makeOnlineOrder(data);
+                    // Then define and call a function to handle the event checkout.session.completed
+                    break;
+                // ... handle other event types
+                default:
+                    console.log(`Unhandled event type ${event.type}`);
+            }
+    
+            // Return a 200 response to acknowledge receipt of the event
+            response.send();
+        })
+    );
+    
+    
 
     cloudinaryCofigration();
 
